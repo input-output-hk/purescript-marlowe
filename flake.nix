dbdfef9d266746984036812c751d13075950cc26
Hernan Rajchert <hrajchert@gmail.com>
2022-12-16 13:14:05 -0300

    Add hydra support

--- flake.nix
+++ flake.nix
@@ -21,81 +21,106 @@
   };
 
   outputs = { self, nixpkgs, pre-commit-hooks, flake-utils, easy-purescript-nix, nixLsp, gitignore, marloweSpec }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          overlays = [ nixLsp.overlays.nil ];
-          pkgs = import nixpkgs { inherit system overlays; };
-          inherit (gitignore.lib) gitignoreSource;
-          inherit (pkgs) git writeShellScriptBin mkShell nodePackages;
+    let
+      inherit (flake-utils.lib) eachSystem system;
+      supportedSystems = [ system.x86_64-linux system.x86_64-darwin ];
 
-          easy-ps = import easy-purescript-nix { inherit pkgs; };
+      base = { evalSystem ? null }: eachSystem supportedSystems
+        (system:
+          let evalSystem' = if evalSystem == null then system else evalSystem; in
+          let
+            evalSystem = evalSystem';
+            overlays = [ nixLsp.overlays.nil ];
+            pkgs = import nixpkgs { inherit system overlays; };
+            inherit (gitignore.lib) gitignoreSource;
+            inherit (pkgs) git writeShellScriptBin mkShell nodePackages;
 
-          src = gitignoreSource ./.;
+            easy-ps = import easy-purescript-nix { inherit pkgs; };
 
-          writeShellScriptBinInRepoRoot = name: script: writeShellScriptBin name ''
-            cd `${git}/bin/git rev-parse --show-toplevel`
-            ${script}
-          '';
+            src = gitignoreSource ./.;
 
-          formatting = import ./nix/formatting.nix {
-            inherit writeShellScriptBinInRepoRoot pkgs easy-ps;
-          };
+            writeShellScriptBinInRepoRoot = name: script: writeShellScriptBin name ''
+              cd `${git}/bin/git rev-parse --show-toplevel`
+              ${script}
+            '';
 
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            inherit src;
-            hooks = {
-              nixpkgs-fmt = {
-                enable = true;
-                excludes = [ ".*spago-packages.nix$" ];
-              };
-              prettier = formatting.prettier-hook;
-              inherit (formatting) purs-tidy-hook dhall-hook;
+            formatting = import ./nix/formatting.nix {
+              inherit writeShellScriptBinInRepoRoot pkgs easy-ps;
             };
-          };
 
-          purescript-marlowe = import ./nix/purescript-marlowe.nix {
-            inherit pkgs src easy-ps writeShellScriptBinInRepoRoot marloweSpecDriver;
-          };
+            pre-commit-check = pre-commit-hooks.lib.${system}.run {
+              inherit src;
+              hooks = {
+                nixpkgs-fmt = {
+                  enable = true;
+                  excludes = [ ".*spago-packages.nix$" ];
+                };
+                prettier = formatting.prettier-hook;
+                inherit (formatting) purs-tidy-hook dhall-hook;
+              };
+            };
 
-          marloweSpecDriver = marloweSpec.packages."${system}"."marlowe-spec-test:exe:marlowe-spec";
-        in
-        {
-          packages.default = purescript-marlowe.marlowe;
-          packages.generateSpagoPackages = purescript-marlowe.generateSpagoPackages;
-          devShell = mkShell {
-            buildInputs = [
-              marloweSpecDriver
-              pkgs.nil
-              pkgs.nodejs
-              nodePackages.prettier
-              nodePackages.bower
-            ] ++
-            (with easy-ps; [
-              dhall-simple
-              purs
-              psa
-              spago
-              spago2nix
-              purs-tidy
-              purescript-language-server
-            ]) ++
-            (with purescript-marlowe; [
-              build
-              clean
-              clean-build
-              test
-              generateSpagoPackages
-              marlowe-spec-client
-            ]) ++
-            (with formatting; [
-              fix-purs-tidy
-              fix-nix-fmt
-              fix-prettier
-            ]
-            );
-            inherit (pre-commit-check) shellHook;
-          };
-        }
-      );
+            purescript-marlowe = import ./nix/purescript-marlowe.nix {
+              inherit pkgs src easy-ps writeShellScriptBinInRepoRoot marloweSpecDriver;
+            };
+
+            marloweSpecDriver = marloweSpec.packages."${system}"."marlowe-spec-test:exe:marlowe-spec";
+
+            packages = {
+              default = purescript-marlowe.marlowe;
+              generateSpagoPackages = purescript-marlowe.generateSpagoPackages;
+            };
+          in
+          {
+            inherit packages;
+            hydraJobs = packages;
+
+            devShell = mkShell {
+              buildInputs = [
+                marloweSpecDriver
+                pkgs.nil
+                pkgs.nodejs
+                nodePackages.prettier
+                nodePackages.bower
+              ] ++
+              (with easy-ps; [
+                dhall-simple
+                purs
+                psa
+                spago
+                spago2nix
+                purs-tidy
+                purescript-language-server
+              ]) ++
+              (with purescript-marlowe; [
+                build
+                clean
+                clean-build
+                test
+                generateSpagoPackages
+                marlowe-spec-client
+              ]) ++
+              (with formatting; [
+                fix-purs-tidy
+                fix-nix-fmt
+                fix-prettier
+              ]
+              );
+              inherit (pre-commit-check) shellHook;
+            };
+          }
+        );
+      hydraSystem = "x86_64-linux";
+      pkgsHydra = nixpkgs.legacyPackages.${hydraSystem};
+      baseHydra = base { evalSystem = hydraSystem; };
+    in
+    base { } // {
+      hydraJobs = baseHydra.hydraJobs // {
+        forceNewEval = pkgsHydra.writeText "forceNewEval" (self.rev or self.lastModified);
+        required = pkgsHydra.releaseTools.aggregate {
+          name = "purescript-marlowe";
+          constituents = builtins.concatMap (system: map (x: "${x}.${system}") (builtins.attrNames baseHydra.hydraJobs)) supportedSystems;
+        };
+      };
+    };
 }
